920106632f0601e05cafb77b34f1e3b4986b9934
mk3008 <mk3008.develop@gmail.com>
2022-12-27 17:15:53 +0900

    Fixed indentation problem in cases expression

--- test/Carbunql.Analysis.Test/CommandTextBuilderTest.cs
+++ test/Carbunql.Analysis.Test/CommandTextBuilderTest.cs
@@ -4,17 +4,17 @@ namespace Carbunql.Analysis.Test;
 
 public class CommandTextBuilderTest
 {
-	private readonly ITestOutputHelper Output;
+    private readonly ITestOutputHelper Output;
 
-	public CommandTextBuilderTest(ITestOutputHelper output)
-	{
-		Output = output;
-	}
+    public CommandTextBuilderTest(ITestOutputHelper output)
+    {
+        Output = output;
+    }
 
-	[Fact]
-	public void Default()
-	{
-		var sql = @"select
+    [Fact]
+    public void Default()
+    {
+        var sql = @"select
     a.table_a_id as id,
     3.14 as val,
     (a.val + b.val) * 2 as calc, 
@@ -25,11 +25,28 @@ from
     inner join table_b b on a.table_a_id = b.table_a_id and b.visible = true
     left join table_c c on a.table_a_id = c.table_a_id";
 
-		var sq = QueryParser.Parse(sql);
-		var frm = new CommandFormatter();
-		var b = new CommandTextBuilder(frm);
+        var sq = QueryParser.Parse(sql);
+        var frm = new CommandFormatter();
+        var b = new CommandTextBuilder(frm);
 
-		var formatsql = b.Execute(sq.GetTokens(null));
-		Output.WriteLine(formatsql);
-	}
+        var formatsql = b.Execute(sq.GetTokens(null));
+        Output.WriteLine(formatsql);
+    }
+
+    [Fact]
+    public void ConcatCase()
+    {
+        var sql = @"select
+concat(
+case when 1=1 then 'a' else 'b' end, 
+case when 2=2 then 'c' else 'd' end
+)";
+
+        var sq = QueryParser.Parse(sql);
+        var frm = new CommandFormatter();
+        var b = new CommandTextBuilder(frm);
+
+        var formatsql = b.Execute(sq.GetTokens(null));
+        Output.WriteLine(formatsql);
+    }
 }
