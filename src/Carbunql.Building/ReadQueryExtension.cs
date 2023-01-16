bf6371a6ed3bc8311846ef60795d8418b26cba63
mk3008 <mk3008.develop@gmail.com>
2023-01-16 21:24:15 +0900

    Added demo pages for ToCTE and ToSubQuery

--- src/Carbunql.Building/ReadQueryExtension.cs
+++ src/Carbunql.Building/ReadQueryExtension.cs
@@ -7,206 +7,226 @@ namespace Carbunql.Building;
 
 public static class ReadQueryExtension
 {
-    public static CTEQuery ToCTE(this IReadQuery source, string alias)
-    {
-        var sq = new CTEQuery();
+	private static CTEQuery ImportCommonTable(this CTEQuery source, CTEQuery tagert)
+	{
+		foreach (var item in tagert.WithClause)
+		{
+			source.WithClause.Add(item);
+		}
+		return source;
+	}
 
-        sq.WithClause.Add(source.ToCommonTable(alias));
+	public static CTEQuery ToCTE(this IReadQuery source, string alias)
+	{
+		var sq = new CTEQuery();
 
-        return sq;
-    }
+		if (source is CTEQuery q) sq.ImportCommonTable(q);
 
-    public static CommonTable ToCommonTable(this IReadQuery source, string alias)
-    {
-        return new CommonTable(new VirtualTable(source), alias);
-    }
+		sq.WithClause.Add(source.ToCommonTable(alias));
 
-    public static SelectableTable ToSelectableTable(this IReadQuery source, string alias)
-    {
-        var t = new VirtualTable(source);
-        return new SelectableTable(t, alias);
-    }
+		return sq;
+	}
 
-    public static FromClause ToFromClause(this IReadQuery source, string alias)
-    {
-        var t = source.ToSelectableTable(alias);
-        return new FromClause(t);
-    }
+	public static CommonTable ToCommonTable(this IReadQuery source, string alias)
+	{
+		return new CommonTable(new VirtualTable(source.GetQuery()), alias);
+	}
 
-    public static (SelectQuery, FromClause) ToSubQuery(this IReadQuery source, string alias)
-    {
-        var sq = new SelectQuery();
-        var f = sq.From(source, alias);
-        return (sq, f);
-    }
+	public static SelectableTable ToSelectableTable(this IReadQuery source, string alias)
+	{
+		var t = new VirtualTable(source);
+		return new SelectableTable(t, alias);
+	}
 
-    public static SelectQuery ToSubQuery(this IReadQuery source, string alias, Predicate<SelectableItem> columnFilter)
-    {
-        var s = source.GetSelectClause();
-        if (s == null) throw new NotSupportedException();
+	public static FromClause ToFromClause(this IReadQuery source, string alias)
+	{
+		var t = source.ToSelectableTable(alias);
+		return new FromClause(t);
+	}
 
-        var sq = new SelectQuery();
-        var f = sq.From(source, alias);
-        foreach (var item in s.Items)
-        {
-            if (!columnFilter(item)) continue;
-            sq.Select(f, item.Alias);
-        }
-        return sq;
-    }
+	public static (SelectQuery, FromClause) ToSubQuery(this IReadQuery source, string alias)
+	{
+		var sq = new SelectQuery();
+		var f = sq.From(source, alias);
+		return (sq, f);
+	}
 
-    public static CreateTableQuery ToCreateTableQuery(this IReadQuery source, string table)
-    {
-        var t = table.ToPhysicalTable();
-        return source.ToCreateTableQuery(t);
-    }
+	public static IReadQuery ToSubQuery(this IReadQuery source, string alias, Predicate<SelectableItem> columnFilter)
+	{
+		var s = source.GetSelectClause();
+		if (s == null) throw new NotSupportedException();
 
-    public static CreateTableQuery ToCreateTableQuery(this IReadQuery source, TableBase table)
-    {
-        return new CreateTableQuery(new CreateTableClause(table))
-        {
-            Parameters = source.GetParameters(),
-            Query = source,
-        };
-    }
+		var sq = new SelectQuery();
+		var f = sq.From(source.GetQuery(), alias);
+		foreach (var item in s.Items)
+		{
+			if (!columnFilter(item)) continue;
+			sq.Select(f, item.Alias);
+		}
 
-    public static SelectableTable ToInsertTable(this IReadQuery source, string table)
-    {
-        var s = source.GetSelectClause();
-        if (s == null)
-        {
-            return new SelectableTable(new PhysicalTable(table), table);
-        }
+		if (source is CTEQuery q)
+		{
+			var cte = new CTEQuery();
+			cte.ImportCommonTable(q);
+			cte.Query = sq;
+			return cte;
+		}
 
-        var vals = new ValueCollection();
-        foreach (var item in s.Items)
-        {
-            vals.Add(new ColumnValue(item.Alias));
-        }
+		return sq;
+	}
 
-        return new SelectableTable(new PhysicalTable(table), table, vals);
-    }
+	public static CreateTableQuery ToCreateTableQuery(this IReadQuery source, string table)
+	{
+		var t = table.ToPhysicalTable();
+		return source.ToCreateTableQuery(t);
+	}
 
-    public static InsertQuery ToInsertQuery(this IReadQuery source, string table)
-    {
-        var t = source.ToInsertTable(table);
-        return source.ToInsertQuery(t);
-    }
+	public static CreateTableQuery ToCreateTableQuery(this IReadQuery source, TableBase table)
+	{
+		return new CreateTableQuery(new CreateTableClause(table))
+		{
+			Parameters = source.GetParameters(),
+			Query = source,
+		};
+	}
 
-    public static InsertQuery ToInsertQuery(this IReadQuery source, SelectableTable table)
-    {
-        return new InsertQuery()
-        {
-            InsertClause = new InsertClause(table),
-            Parameters = source.GetParameters(),
-            Query = source,
-        };
-    }
+	public static SelectableTable ToInsertTable(this IReadQuery source, string table)
+	{
+		var s = source.GetSelectClause();
+		if (s == null)
+		{
+			return new SelectableTable(new PhysicalTable(table), table);
+		}
 
-    /// <summary>
-    /// set val = queryAlias.val
-    /// </summary>
-    /// <param name="source"></param>
-    /// <param name="keys"></param>
-    /// <param name="alias"></param>
-    /// <param name="queryAlias"></param>
-    /// <returns></returns>
-    /// <exception cref="NotSupportedException"></exception>
-    private static SetClause ToSetClause(this IReadQuery source, IEnumerable<string> keys, string alias, string queryAlias)
-    {
-        var s = source.GetSelectClause();
-        if (s == null) throw new NotSupportedException();
-        var cols = s.Items.Where(x => !keys.Contains(x.Alias)).Select(x => x.Alias).ToList();
+		var vals = new ValueCollection();
+		foreach (var item in s.Items)
+		{
+			vals.Add(new ColumnValue(item.Alias));
+		}
 
-        var clause = new SetClause();
-        foreach (var item in cols)
-        {
-            var c = new ColumnValue(alias, item);
-            c.Equal(new ColumnValue(queryAlias, item));
-            clause.Add(c);
-        };
-        return clause;
-    }
+		return new SelectableTable(new PhysicalTable(table), table, vals);
+	}
 
-    /// <summary>
-    /// where alias.key = queryAlias.key
-    /// </summary>
-    /// <param name="keys"></param>
-    /// <param name="alias"></param>
-    /// <param name="queryAlias"></param>
-    /// <returns></returns>
-    /// <exception cref="Exception"></exception>
-    private static WhereClause ToWhereClauseAsUpdate(IEnumerable<string> keys, string alias, string queryAlias)
-    {
-        ValueBase? cnd = null;
-        foreach (var item in keys)
-        {
-            if (cnd == null)
-            {
-                cnd = new ColumnValue(alias, item);
-            }
-            else
-            {
-                cnd.And(new ColumnValue(alias, item));
-            }
-            cnd.Equal(new ColumnValue(queryAlias, item));
-        }
-        if (cnd == null) throw new Exception();
-        return cnd.ToWhereClause();
-    }
+	public static InsertQuery ToInsertQuery(this IReadQuery source, string table)
+	{
+		var t = source.ToInsertTable(table);
+		return source.ToInsertQuery(t);
+	}
 
-    public static UpdateQuery ToUpdateQuery(this IReadQuery source, string table, IEnumerable<string> keys)
-    {
-        var t = table.ToPhysicalTable().ToSelectable();
-        return source.ToUpdateQuery(t, keys);
-    }
+	public static InsertQuery ToInsertQuery(this IReadQuery source, SelectableTable table)
+	{
+		return new InsertQuery()
+		{
+			InsertClause = new InsertClause(table),
+			Parameters = source.GetParameters(),
+			Query = source,
+		};
+	}
 
-    public static UpdateQuery ToUpdateQuery(this IReadQuery source, SelectableTable table, IEnumerable<string> keys)
-    {
-        var queryAlias = "q";
+	/// <summary>
+	/// set val = queryAlias.val
+	/// </summary>
+	/// <param name="source"></param>
+	/// <param name="keys"></param>
+	/// <param name="alias"></param>
+	/// <param name="queryAlias"></param>
+	/// <returns></returns>
+	/// <exception cref="NotSupportedException"></exception>
+	private static SetClause ToSetClause(this IReadQuery source, IEnumerable<string> keys, string alias, string queryAlias)
+	{
+		var s = source.GetSelectClause();
+		if (s == null) throw new NotSupportedException();
+		var cols = s.Items.Where(x => !keys.Contains(x.Alias)).Select(x => x.Alias).ToList();
 
-        return new UpdateQuery()
-        {
-            UpdateClause = new UpdateClause(table),
-            SetClause = source.ToSetClause(keys, table.Alias, queryAlias),
-            Parameters = source.GetParameters(),
-            FromClause = source.ToFromClause(queryAlias),
-            WhereClause = ToWhereClauseAsUpdate(keys, table.Alias, queryAlias),
-        };
-    }
+		var clause = new SetClause();
+		foreach (var item in cols)
+		{
+			var c = new ColumnValue(alias, item);
+			c.Equal(new ColumnValue(queryAlias, item));
+			clause.Add(c);
+		};
+		return clause;
+	}
 
-    public static DeleteQuery ToDeleteQuery(this IReadQuery source, string table, IEnumerable<string> keys)
-    {
-        var t = table.ToPhysicalTable().ToSelectable();
-        return source.ToDeleteQuery(t, keys);
-    }
+	/// <summary>
+	/// where alias.key = queryAlias.key
+	/// </summary>
+	/// <param name="keys"></param>
+	/// <param name="alias"></param>
+	/// <param name="queryAlias"></param>
+	/// <returns></returns>
+	/// <exception cref="Exception"></exception>
+	private static WhereClause ToWhereClauseAsUpdate(IEnumerable<string> keys, string alias, string queryAlias)
+	{
+		ValueBase? cnd = null;
+		foreach (var item in keys)
+		{
+			if (cnd == null)
+			{
+				cnd = new ColumnValue(alias, item);
+			}
+			else
+			{
+				cnd.And(new ColumnValue(alias, item));
+			}
+			cnd.Equal(new ColumnValue(queryAlias, item));
+		}
+		if (cnd == null) throw new Exception();
+		return cnd.ToWhereClause();
+	}
 
-    private static WhereClause ToWhereClauseAsDelete(this IReadQuery source, IEnumerable<string> keys, string alias, string queryAlias)
-    {
-        var ks = keys.ToList();
+	public static UpdateQuery ToUpdateQuery(this IReadQuery source, string table, IEnumerable<string> keys)
+	{
+		var t = table.ToPhysicalTable().ToSelectable();
+		return source.ToUpdateQuery(t, keys);
+	}
 
-        var cnd = new ValueCollection();
-        ks.ForEach(x => cnd.Add(new ColumnValue(alias, x)));
-        if (cnd == null) throw new Exception();
+	public static UpdateQuery ToUpdateQuery(this IReadQuery source, SelectableTable table, IEnumerable<string> keys)
+	{
+		var queryAlias = "q";
 
-        var sq = source.ToSubQuery(queryAlias, (x) =>
-        {
-            if (ks.Where(k => k.AreEqual(x.Alias)).Any()) return true;
-            return false;
-        });
-        var exp = new InExpression(cnd.ToBracket(), sq.ToValue());
-        return exp.ToWhereClause();
-    }
+		return new UpdateQuery()
+		{
+			UpdateClause = new UpdateClause(table),
+			SetClause = source.ToSetClause(keys, table.Alias, queryAlias),
+			Parameters = source.GetParameters(),
+			FromClause = source.ToFromClause(queryAlias),
+			WhereClause = ToWhereClauseAsUpdate(keys, table.Alias, queryAlias),
+		};
+	}
 
-    public static DeleteQuery ToDeleteQuery(this IReadQuery source, SelectableTable table, IEnumerable<string> keys)
-    {
-        var queryAlias = "q";
+	public static DeleteQuery ToDeleteQuery(this IReadQuery source, string table, IEnumerable<string> keys)
+	{
+		var t = table.ToPhysicalTable().ToSelectable();
+		return source.ToDeleteQuery(t, keys);
+	}
 
-        return new DeleteQuery()
-        {
-            DeleteClause = new DeleteClause(table),
-            Parameters = source.GetParameters(),
-            WhereClause = source.ToWhereClauseAsDelete(keys, table.Alias, queryAlias),
-        };
-    }
+	private static WhereClause ToWhereClauseAsDelete(this IReadQuery source, IEnumerable<string> keys, string alias, string queryAlias)
+	{
+		var ks = keys.ToList();
+
+		var cnd = new ValueCollection();
+		ks.ForEach(x => cnd.Add(new ColumnValue(alias, x)));
+		if (cnd == null) throw new Exception();
+
+		var sq = source.ToSubQuery(queryAlias, (x) =>
+		{
+			if (ks.Where(k => k.AreEqual(x.Alias)).Any()) return true;
+			return false;
+		});
+		var exp = new InExpression(cnd.ToBracket(), sq.ToValue());
+		return exp.ToWhereClause();
+	}
+
+	public static DeleteQuery ToDeleteQuery(this IReadQuery source, SelectableTable table, IEnumerable<string> keys)
+	{
+		var queryAlias = "q";
+
+		return new DeleteQuery()
+		{
+			DeleteClause = new DeleteClause(table),
+			Parameters = source.GetParameters(),
+			WhereClause = source.ToWhereClauseAsDelete(keys, table.Alias, queryAlias),
+		};
+	}
 }
--- src/Carbunql/IReadQuery.cs
+++ src/Carbunql/IReadQuery.cs
@@ -1,10 +1,19 @@
 ﻿using Carbunql.Clauses;
+using Carbunql.Values;
 
 namespace Carbunql;
 
 public interface IReadQuery : IQueryCommandable
 {
-    SelectClause? GetSelectClause();
+	SelectClause? GetSelectClause();
 
-    ReadQuery GetQuery();
+	ReadQuery GetQuery();
+}
+
+public static class IReadQueryExtension
+{
+	public static ValueBase ToValue(this IReadQuery source)
+	{
+		return new QueryContainer(source.GetQuery());
+	}
 }
