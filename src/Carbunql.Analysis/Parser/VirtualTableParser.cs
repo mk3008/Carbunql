419cc77a5733521d7a1a83ae64c6c16f7df9f851
mk3008 <mk3008.develop@gmail.com>
2023-01-24 21:22:04 +0900

    Added VirtualTableParser

---
+++ src/Carbunql.Analysis/Parser/VirtualTableParser.cs
@@ -0,0 +1,29 @@
+﻿using Carbunql.Extensions;
+using Carbunql.Tables;
+using System.ComponentModel.DataAnnotations;
+
+namespace Carbunql.Analysis.Parser;
+
+public class VirtualTableParser
+{
+	public static VirtualTable Parse(ITokenReader r)
+	{
+		r.ReadToken("(");
+		var ir = new InnerTokenReader(r);
+
+		var first = ir.PeekRawToken();
+
+		if (first == null) throw new NotSupportedException();
+
+		//virtualTable
+		if (first.AreEqual("select"))
+		{
+			return new VirtualTable(SelectQueryParser.Parse(ir));
+		}
+		else if (first.AreEqual("values"))
+		{
+			return new VirtualTable(ValuesClauseParser.Parse(ir));
+		}
+		throw new NotSupportedException();
+	}
+}
