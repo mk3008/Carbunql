453651ec5a90761cd9433720f597813700cc7cbb
mk3008 <mk3008.develop@gmail.com>
2023-02-10 23:25:19 +0900

    #79 Aligned PartitionBy clause formatting with OrderBy clause

--- src/Carbunql/CommandFormatter.cs
+++ src/Carbunql/CommandFormatter.cs
@@ -7,67 +7,68 @@ namespace Carbunql;
 
 public class CommandFormatter
 {
-    public Action<string>? Logger { get; set; }
+	public Action<string>? Logger { get; set; }
 
-    public virtual bool IsLineBreakOnBeforeWriteToken(Token token)
-    {
-        if (token.Text.Equals(",") && token.Sender is Relation) return false;
+	public virtual bool IsLineBreakOnBeforeWriteToken(Token token)
+	{
+		if (token.Text.Equals(",") && token.Sender is Relation) return false;
 
-        if (!token.Text.IsEqualNoCase("on") && token.Sender is Relation) return true;
-        if (token.Text.IsEqualNoCase("else") || token.Text.IsEqualNoCase("when")) return true;
-        if (token.Text.IsEqualNoCase("and"))
-        {
-            if (token.Sender is BetweenExpression) return false;
-            if (token.Parent != null && token.Parent.Sender is WhereClause) return true;
-            if (token.Parent != null && token.Parent.Sender is HavingClause) return true;
-            return false;
-        }
+		if (!token.Text.IsEqualNoCase("on") && token.Sender is Relation) return true;
+		if (token.Text.IsEqualNoCase("else") || token.Text.IsEqualNoCase("when")) return true;
+		if (token.Text.IsEqualNoCase("and"))
+		{
+			if (token.Sender is BetweenExpression) return false;
+			if (token.Parent != null && token.Parent.Sender is WhereClause) return true;
+			if (token.Parent != null && token.Parent.Sender is HavingClause) return true;
+			return false;
+		}
 
-        return false;
-    }
+		return false;
+	}
 
-    public virtual bool IsLineBreakOnAfterWriteToken(Token token)
-    {
-        if (token.Sender is OperatableQuery) return true;
+	public virtual bool IsLineBreakOnAfterWriteToken(Token token)
+	{
+		if (token.Sender is OperatableQuery) return true;
 
-        if (token.Text.Equals(","))
-        {
-            if (token.Sender is WithClause) return true;
-            if (token.Sender is SelectClause) return true;
-            if (token.Sender is Relation) return true;
-            if (token.Sender is GroupClause) return true;
-            if (token.Sender is OrderClause) return true;
-            if (token.Sender is ValuesQuery) return true;
-            if (token.Sender is SetClause) return true;
-        }
+		if (token.Text.Equals(","))
+		{
+			if (token.Sender is WithClause) return true;
+			if (token.Sender is SelectClause) return true;
+			if (token.Sender is Relation) return true;
+			if (token.Sender is GroupClause) return true;
+			if (token.Sender is OrderClause) return true;
+			if (token.Sender is ValuesQuery) return true;
+			if (token.Sender is SetClause) return true;
+			if (token.Sender is PartitionClause) return true;
+		}
 
-        return false;
-    }
+		return false;
+	}
 
-    public virtual bool IsIncrementIndentOnBeforeWriteToken(Token token)
-    {
-        if (token.Sender is OperatableQuery) return false;
+	public virtual bool IsIncrementIndentOnBeforeWriteToken(Token token)
+	{
+		if (token.Sender is OperatableQuery) return false;
 
-        if (token.Text.Equals("(") && token.IsReserved == false) return false;
-        if (token.Parent != null && token.Parent.Sender is ValuesQuery) return false;
-        if (token.Sender is FunctionValue) return false;
-        if (token.Sender is FunctionTable) return false;
-        if (token.Text.Equals("over")) return false;
+		if (token.Text.Equals("(") && token.IsReserved == false) return false;
+		if (token.Parent != null && token.Parent.Sender is ValuesQuery) return false;
+		if (token.Sender is FunctionValue) return false;
+		if (token.Sender is FunctionTable) return false;
+		if (token.Text.Equals("over")) return false;
 
-        return true;
-    }
+		return true;
+	}
 
-    public virtual bool IsDecrementIndentOnBeforeWriteToken(Token token)
-    {
-        if (token.Parent == null) return true;
+	public virtual bool IsDecrementIndentOnBeforeWriteToken(Token token)
+	{
+		if (token.Parent == null) return true;
 
-        if (token.Text.Equals(")") && token.IsReserved == false) return false;
+		if (token.Text.Equals(")") && token.IsReserved == false) return false;
 
-        if (token.Parent.Sender is ValuesQuery) return false;
-        if (token.Sender is FunctionValue) return false;
-        if (token.Sender is FunctionTable) return false;
-        if (token.Text.Equals(")") && token.Parent.Text.IsEqualNoCase("over")) return true;
+		if (token.Parent.Sender is ValuesQuery) return false;
+		if (token.Sender is FunctionValue) return false;
+		if (token.Sender is FunctionTable) return false;
+		if (token.Text.Equals(")") && token.Parent.Text.IsEqualNoCase("over")) return true;
 
-        return true;
-    }
+		return true;
+	}
 }
