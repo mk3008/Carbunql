d9f9de392bd3c9cc1e2147275dd969c22f294d65
mk3008 <mk3008.develop@gmail.com>
2023-02-20 22:02:00 +0900

    Added ToUpdateQuery demo page.

--- test/Carbunql.Building.Test/UpdateTest.cs
+++ test/Carbunql.Building.Test/UpdateTest.cs
@@ -7,38 +7,38 @@ namespace Carbunql.Building.Test;
 
 public class UpdateTest
 {
-    private readonly QueryCommandMonitor Monitor;
+	private readonly QueryCommandMonitor Monitor;
 
-    public UpdateTest(ITestOutputHelper output)
-    {
-        Monitor = new QueryCommandMonitor(output);
-    }
+	public UpdateTest(ITestOutputHelper output)
+	{
+		Monitor = new QueryCommandMonitor(output);
+	}
 
-    [Fact]
-    public void UpdateQuery_Alias()
-    {
-        var sql = "select a.id, a.sub_id, a.v1, a.v2 from table as a";
-        var q = QueryParser.Parse(sql);
+	[Fact]
+	public void UpdateQuery_Alias()
+	{
+		var sql = "select a.id, a.sub_id, a.v1, a.v2 from table as a";
+		var q = QueryParser.Parse(sql);
 
-        var uq = q.ToUpdateQuery("new_table".ToPhysicalTable().ToSelectable("t"), new[] { "id", "sub_id" });
-        Monitor.Log(uq);
+		var uq = q.ToUpdateQuery("new_table".ToPhysicalTable().ToSelectable("t"), new[] { "id", "sub_id" });
+		Monitor.Log(uq);
 
-        var lst = uq.GetTokens().ToList();
+		var lst = uq.GetTokens().ToList();
 
-        Assert.Equal(61, lst.Count());
-    }
+		Assert.Equal(61, lst.Count());
+	}
 
-    [Fact]
-    public void UpdateQuery()
-    {
-        var sql = "select a.id, a.sub_id, a.v1, a.v2 from table as a";
-        var q = QueryParser.Parse(sql);
+	[Fact]
+	public void UpdateQuery()
+	{
+		var sql = "select a.id, a.sub_id, a.v1, a.v2 from table as a";
+		var q = QueryParser.Parse(sql);
 
-        var uq = q.ToUpdateQuery("new_table", new[] { "id", "sub_id" });
-        Monitor.Log(uq);
+		var uq = q.ToUpdateQuery("new_table", new[] { "id", "sub_id" });
+		Monitor.Log(uq);
 
-        var lst = uq.GetTokens().ToList();
+		var lst = uq.GetTokens().ToList();
 
-        Assert.Equal(59, lst.Count());
-    }
+		Assert.Equal(61, lst.Count());
+	}
 }
