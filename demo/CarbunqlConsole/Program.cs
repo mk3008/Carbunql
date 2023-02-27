08cae8c72ae38ad1986f2798f7a4aee9fdf5d79c
mk3008 <mk3008.develop@gmail.com>
2023-02-27 21:47:39 +0900

    Added source code to CRUD demo page

---
+++ demo/CarbunqlConsole/Program.cs
@@ -0,0 +1,10 @@
+﻿using Carbunql;
+using Carbunql.Building;
+
+var sq = new SelectQuery("select * from table_a as a");
+var q = sq.ToDeleteQuery("destination", new[] { "id" });
+Console.WriteLine(q.ToCommand().CommandText);
+
+Console.WriteLine();
+Console.WriteLine("press enter key");
+Console.ReadLine();
