5957061c4fdbb1b655b6229217157d249390e6f3
mk3008 <mk3008.develop@gmail.com>
2023-02-27 22:00:52 +0900

    Added sample code to CTE demo page

--- demo/CarbunqlConsole/Program.cs
+++ demo/CarbunqlConsole/Program.cs
@@ -1,9 +1,19 @@
 ﻿using Carbunql;
 using Carbunql.Building;
 
-var sq = new SelectQuery("select * from table_a as a");
-var q = sq.ToDeleteQuery("destination", new[] { "id" });
-Console.WriteLine(q.ToCommand().CommandText);
+var tmp = new SelectQuery("select * from table_a as a");
+
+//Convert select query to Common table
+//Return value is SelectQuery class and Common table class
+var (sq, ct) = tmp.ToCTE("alias");
+
+//Set common table to From clause
+var t = sq.From(ct);
+
+//Select all columns of the Common table
+sq.Select(t);
+
+Console.WriteLine(sq.ToCommand().CommandText);
 
 Console.WriteLine();
 Console.WriteLine("press enter key");
