7a2d3bd52d757107fb1f3ba41747f047bc81ca88
mk3008 <mk3008.develop@gmail.com>
2023-02-28 20:35:13 +0900

    Added sample source code.

--- demo/CarbunqlConsole/Program.cs
+++ demo/CarbunqlConsole/Program.cs
@@ -2,16 +2,7 @@
 using Carbunql.Building;
 
 var tmp = new SelectQuery("select * from table_a as a");
-
-//Convert select query to Common table
-//Return value is SelectQuery class and Common table class
-var (sq, ct) = tmp.ToCTE("alias");
-
-//Set common table to From clause
-var t = sq.From(ct);
-
-//Select all columns of the Common table
-sq.Select(t);
+var sq = tmp.ToCountQuery();
 
 Console.WriteLine(sq.ToCommand().CommandText);
 
