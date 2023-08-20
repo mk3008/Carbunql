9110f48bffdfe3e634204f94a422b17f5d357e35
mk3008 <mk3008.develop@gmail.com>
2023-08-20 12:11:57 +0900

    #164 Project linkage verification of SourceGenerator

---
+++ src/Carbunql.SourceGenerator/SourceGenerator.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.CodeAnalysis;
+
+namespace Carbunql.SourceGenerator;
+
+[Generator]
+public class SourceGenerator : IIncrementalGenerator
+{
+	public void Initialize(IncrementalGeneratorInitializationContext context)
+	{
+		throw new Exception("run source generator");
+	}
+}
---
+++ test/Carbunql.SourceGenerator.Test/Carbunql.SourceGenerator.Test/UnitTest1.cs
@@ -0,0 +1,11 @@
+namespace Carbunql.SourceGenerator.Test
+{
+	public class UnitTest1
+	{
+		[Fact]
+		public void Test1()
+		{
+
+		}
+	}
+}
