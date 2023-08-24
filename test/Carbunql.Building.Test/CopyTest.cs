df8525d9d76ebfafa73c43e815f1ecd85b4bbc6c
mk3008 <mk3008.develop@gmail.com>
2023-08-24 23:55:35 +0900

    #168 Added a serializer class specifically for the IReadQuery interface

--- test/Carbunql.Building.Test/CopyTest.cs
+++ test/Carbunql.Building.Test/CopyTest.cs
@@ -1,4 +1,5 @@
-﻿using MessagePack;
+﻿using Carbunql.Analysis;
+using MessagePack;
 using Xunit.Abstractions;
 
 namespace Carbunql.Building.Test;
@@ -22,6 +23,39 @@ public class CopyTest
 	{
 		var sq = new SelectQuery("select a.column_1 as c1 from table_a as a");
 		var actual = sq.DeepCopy();
+
+		Assert.Equal(TruncateControlString(sq.ToText()), TruncateControlString(actual!.ToText()));
+		Assert.NotEqual(sq, actual);
+	}
+
+	[Fact]
+	public void Serialize_SelectQuery()
+	{
+		var sq = new SelectQuery("select a.column_1 as c1 from table_a as a");
+		var json = Serializer.Serialize(sq);
+		var actual = Serializer.Deserialize<SelectQuery>(json);
+
+		Assert.Equal(TruncateControlString(sq.ToText()), TruncateControlString(actual!.ToText()));
+		Assert.NotEqual(sq, actual);
+	}
+
+	[Fact]
+	public void Serialize()
+	{
+		var sq = new SelectQuery("select a.column_1 as c1 from table_a as a");
+		var json = Serializer.Serialize(sq);
+		var actual = Serializer.Deserialize(json);
+
+		Assert.Equal(TruncateControlString(sq.ToText()), TruncateControlString(actual!.ToText()));
+		Assert.NotEqual(sq, actual);
+	}
+
+	[Fact]
+	public void DeserializeFromByte()
+	{
+		var sq = new SelectQuery("select a.column_1 as c1 from table_a as a");
+		var json = new byte[] { 146, 0, 154, 144, 145, 146, 146, 6, 147, 192, 161, 97, 168, 99, 111, 108, 117, 109, 110, 95, 49, 162, 99, 49, 146, 147, 146, 1, 148, 194, 192, 167, 116, 97, 98, 108, 101, 95, 97, 192, 161, 97, 192, 192, 192, 192, 192, 192, 192, 192, 128 };
+		var actual = Serializer.Deserialize(json);
 
 		Assert.Equal(TruncateControlString(sq.ToText()), TruncateControlString(actual!.ToText()));
 		Assert.NotEqual(sq, actual);
