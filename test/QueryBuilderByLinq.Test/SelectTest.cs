9bafc4a9a6f11cab9a8ef85184e0c73f6034b3c0
mk3008 <mk3008.develop@gmail.com>
2023-10-17 22:39:31 +0900

    #253 Added constant test.

--- test/QueryBuilderByLinq.Test/SelectTest.cs
+++ test/QueryBuilderByLinq.Test/SelectTest.cs
@@ -35,6 +35,42 @@ FROM
 	}
 
 	[Fact]
+	public void ConstValue()
+	{
+		var query = from a in From<table_a>()
+					select new
+					{
+						v1 = 1,
+						v2 = "a",
+						v3 = new DateTime(2001, 2, 3),
+						v4 = DateTime.Now,
+						v5 = true,
+						v6 = 3.14,
+						v7 = 1 + 2 + 3,
+						v8 = a.value * 10
+					};
+		var sq = query.ToQueryAsPostgres();
+
+		Monitor.Log(sq);
+
+		var sql = @"
+SELECT
+    1 AS v1,
+    'a' AS v2,
+    CAST('2001/02/03 0:00:00' AS timestamp) AS v3,
+    current_timestamp AS v4,
+    True AS v5,
+    3.14 AS v6,
+    6 AS v7,
+    a.value * 10 AS v8
+FROM
+    table_a AS a";
+
+		Assert.Equal(45, sq.GetTokens().ToList().Count);
+		Assert.Equal(sql.ToValidateText(), sq.ToText().ToValidateText());
+	}
+
+	[Fact]
 	public void SelectAll()
 	{
 		var query = from a in From<table_a>() select a;
