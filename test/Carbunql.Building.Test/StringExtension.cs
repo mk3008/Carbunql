3e4a1c407b0510d4b34449035f7a89486536bab8
mk3008 <mk3008.develop@gmail.com>
2023-11-25 09:43:41 +0900

    #274 CreateTableQuery class enhancements
    It is now possible to generate a query that selects the created table.

---
+++ test/Carbunql.Building.Test/StringExtension.cs
@@ -0,0 +1,13 @@
+﻿namespace Carbunql.Building.Test;
+
+public static class StringExtension
+{
+	public static string ToValidateText(this string text)
+	{
+		return text.Replace("\r", "")
+			.Replace("\n", "")
+			.Replace(" ", "")
+			.Replace("\t", "")
+			.ToLower();
+	}
+}
