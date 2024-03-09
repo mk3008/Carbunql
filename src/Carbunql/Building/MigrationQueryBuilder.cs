dc5d458f190211d039fb7b959a11ed4adce82726
mk3008 <mk3008.develop@gmail.com>
2024-03-09 12:49:41 +0900

    Add MigrationQueryBuilder

---
+++ src/Carbunql/Building/MigrationQueryBuilder.cs
@@ -0,0 +1,12 @@
+﻿using Carbunql.Analysis;
+
+namespace Carbunql.Building;
+
+public class MigrationQueryBuilder
+{
+	public static DefinitionQuerySet Execute(string expectsql, string actualsql)
+	{
+		var actual = DefinitionQuerySetParser.Parse(actualsql);
+		return actual.GenerateMigrationQuery(expectsql);
+	}
+}
