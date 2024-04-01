d9c3097d56c7aa71824fb4686a85bcd108645b7a
mk3008 <mk3008.develop@gmail.com>
2024-04-02 07:58:23 +0900

    Code cleanup
    Delete unnecessary using

--- test/Carbunql.Building.Test/InsertQueryTest.cs
+++ test/Carbunql.Building.Test/InsertQueryTest.cs
@@ -1,9 +1,4 @@
 ﻿using Carbunql.Analysis;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using Xunit.Abstractions;
 
 namespace Carbunql.Building.Test;
