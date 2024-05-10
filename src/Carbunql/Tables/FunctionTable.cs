78d058bcb10eda32a7a74dcc9fa746bbdb0c63ca
mk3008 <mk3008.develop@gmail.com>
2024-05-10 20:17:14 +0900

    Add comment
    Tables only

--- src/Carbunql/Tables/FunctionTable.cs
+++ src/Carbunql/Tables/FunctionTable.cs
@@ -4,21 +4,36 @@ using MessagePack;
 
 namespace Carbunql.Tables;
 
+/// <summary>
+/// Represents a function table.
+/// </summary>
 [MessagePackObject(keyAsPropertyName: true)]
 public class FunctionTable : TableBase
 {
+    /// <summary>
+    /// Initializes a new instance of the <see cref="FunctionTable"/> class.
+    /// </summary>
     public FunctionTable()
     {
         Name = string.Empty;
         Argument = null!;
     }
 
+    /// <summary>
+    /// Initializes a new instance of the <see cref="FunctionTable"/> class with the specified name.
+    /// </summary>
+    /// <param name="name">The name of the function table.</param>
     public FunctionTable(string name)
     {
         Name = name;
         Argument = new ValueCollection();
     }
 
+    /// <summary>
+    /// Initializes a new instance of the <see cref="FunctionTable"/> class with the specified name and arguments.
+    /// </summary>
+    /// <param name="name">The name of the function table.</param>
+    /// <param name="args">The arguments of the function table.</param>
     public FunctionTable(string name, ValueBase args)
     {
         Name = name;
@@ -28,10 +43,17 @@ public class FunctionTable : TableBase
         };
     }
 
+    /// <summary>
+    /// Gets or sets the name of the function.
+    /// </summary>
     public string Name { get; init; }
 
+    /// <summary>
+    /// Gets or sets the arguments of the function.
+    /// </summary>
     public ValueCollection Argument { get; init; }
 
+    /// <inheritdoc/>
     public override IEnumerable<Token> GetTokens(Token? parent)
     {
         yield return Token.Reserved(this, parent, Name);
@@ -42,13 +64,18 @@ public class FunctionTable : TableBase
         yield return Token.ReservedBracketEnd(this, parent);
     }
 
+    /// <summary>
+    /// Gets or sets the list of parameters associated with the function.
+    /// </summary>
     public List<QueryParameter> Parameters { get; set; } = new();
 
+    /// <inheritdoc/>
     public override IEnumerable<QueryParameter> GetParameters()
     {
         return Parameters;
     }
 
+    /// <inheritdoc/>
     public override IEnumerable<SelectQuery> GetInternalQueries()
     {
         foreach (var item in Argument.GetInternalQueries())
@@ -57,6 +84,7 @@ public class FunctionTable : TableBase
         }
     }
 
+    /// <inheritdoc/>
     public override IEnumerable<PhysicalTable> GetPhysicalTables()
     {
         foreach (var item in Argument.GetPhysicalTables())
@@ -65,6 +93,7 @@ public class FunctionTable : TableBase
         }
     }
 
+    /// <inheritdoc/>
     public override IEnumerable<CommonTable> GetCommonTables()
     {
         foreach (var item in Argument.GetCommonTables())
