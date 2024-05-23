5455d50aa84666eea27361522e67bb607a65815e
mk3008 <mk3008.develop@gmail.com>
2024-05-24 07:44:40 +0900

    Organizing test classes
    Moved the compilation tests to a separate class.

--- test/Carbunql.TypeSafe.Test/CompileTest.cs
+++ test/Carbunql.TypeSafe.Test/CompileTest.cs
@@ -1,130 +1,102 @@
-﻿using Carbunql.Clauses;
-using System.Runtime.InteropServices;
-using Xunit.Abstractions;
+﻿using Xunit.Abstractions;
 
 namespace Carbunql.TypeSafe.Test;
 
-public class SubQueryTest
+public class CompileTest
 {
-    public SubQueryTest(ITestOutputHelper output)
+    public CompileTest(ITestOutputHelper output)
     {
         Output = output;
     }
 
     private ITestOutputHelper Output { get; }
 
-    private FluentSelectQuery SelectOrderById_NoType(int id)
+    [Fact]
+    public void Compile_SelectAll()
     {
         var o = Sql.DefineTable<order>();
-        var query = Sql.From(() => o)
-            .Where(() => o.store_id == id);
-        return query;
-    }
 
-    private FluentSelectQuery<order> SelectOrderById(int id)
-    {
-        var o = Sql.DefineTable<order>();
-        var query = Sql.From(() => o)
-            .Where(() => o.store_id == id);
-        return query.Compile<order>();
-    }
-
-    private FluentSelectQuery<order> SelectOrder()
-    {
-        var o = Sql.DefineTable<order>();
-        var query = Sql.From(() => o);
-        return query.Compile<order>();
-    }
-
-    [Fact]
-    public void SubQuery_NoType()
-    {
-        var o = Sql.DefineTable<order>(SelectOrderById_NoType(1));
-
-        var query = Sql.From(() => o)
-                .Select(() => new { o.store_id });
-
-        var actual = query.ToText();
-        Output.WriteLine(actual);
-
-        var expect = @"/*
-  :id = 1
-*/
-SELECT
-    o.store_id
-FROM
-    (
-        SELECT
-            *
-        FROM
-            order AS o
-        WHERE
-            o.store_id = :id
-    ) AS o";
-
-        Assert.Equal(expect, actual, true, true, true);
-    }
-
-    [Fact]
-    public void SubQuery()
-    {
-        var o = Sql.DefineTable(SelectOrderById(1));
-
-        var query = Sql.From(() => o)
-                .Select(() => new { o.store_id });
-
-        var actual = query.ToText();
-        Output.WriteLine(actual);
-
-        var expect = @"/*
-  :id = 1
-*/
-SELECT
-    o.store_id
-FROM
-    (
-        SELECT
-            *
-        FROM
-            order AS o
-        WHERE
-            o.store_id = :id
-    ) AS o";
-
-        Assert.Equal(expect, actual, true, true, true);
-    }
-
-    [Fact]
-    public void SubQuery_Injection()
-    {
-        var o = Sql.DefineTable(() =>
-        {
-            var x = Sql.DefineTable(SelectOrder());
-            return Sql.From(() => x).Where(() => x.store_id == 1).Compile<order>();
-        });
-
-        var query = Sql.From(() => o)
-                .Select(() => new { o.store_id });
+        var query = Sql.From(() => o).Compile<order>();
 
         var actual = query.ToText();
         Output.WriteLine(actual);
 
         var expect = @"SELECT
-    o.store_id
+    *
 FROM
-    (
-        SELECT
-            *
-        FROM
-            (
-                SELECT
-                    *
-                FROM
-                    order AS o
-            ) AS x
-        WHERE
-            x.store_id = 1
-    ) AS o";
+    order AS o";
+
+        Assert.Equal(expect, actual, true, true, true);
+    }
+
+    [Fact]
+    public void Compile_SelectAll_Exception()
+    {
+        var o = Sql.DefineTable<order>();
+
+        var query = Sql.From(() => o);
+
+        var ex = Assert.Throws<InvalidProgramException>(() => query.Compile<order_detail>());
+        Output.WriteLine(ex.Message);
+
+        Assert.Equal("The select query is not compatible with 'order_detail'. Expect: order_detail, Actual: order", ex.Message);
+    }
+
+    [Fact]
+    public void Compile_Excess()
+    {
+        var o = Sql.DefineTable<order>();
+
+        var query = Sql.From(() => o).Select(() => new { o.order_id, o.store_id, o.order_date, o.customer_name, memo = "test" }).Compile<order>();
+
+        var actual = query.ToText();
+        Output.WriteLine(actual);
+
+        var expect = @"/*
+  :p0 = 'test'
+*/
+SELECT
+    o.order_id,
+    o.store_id,
+    o.order_date,
+    o.customer_name,
+    :p0 AS memo
+FROM
+    order AS o";
+
+        Assert.Equal(expect, actual, true, true, true);
+    }
+
+    [Fact]
+    public void Compile_Undersized()
+    {
+        var o = Sql.DefineTable<order>();
+
+        var query = Sql.From(() => o).Select(() => new { o.order_id });
+
+        var ex = Assert.Throws<InvalidProgramException>(() => query.Compile<order>());
+        Output.WriteLine(ex.Message);
+
+        Assert.Equal("The select query is not compatible with 'order'. The following columns are missing: order_date, customer_name, store_id", ex.Message);
+    }
+
+    [Fact]
+    public void Compile_ForceCorrect()
+    {
+        var o = Sql.DefineTable<order>();
+
+        var query = Sql.From(() => o).Select(() => new { o.order_id }).Compile<order>(true);
+
+        var actual = query.ToText();
+        Output.WriteLine(actual);
+
+        var expect = @"SELECT
+    o.order_id,
+    CAST(null AS timestamp) AS order_date,
+    CAST(null AS text) AS customer_name,
+    CAST(null AS integer) AS store_id
+FROM
+    order AS o";
 
         Assert.Equal(expect, actual, true, true, true);
     }
