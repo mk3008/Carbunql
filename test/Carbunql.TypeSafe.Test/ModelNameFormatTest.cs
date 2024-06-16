b8014f4a1ffcd7ea2bd7387255dd82bc505b980b
mk3008 <mk3008.develop@gmail.com>
2024-06-16 09:28:43 +0900

    Added test code to reproduce the problem

---
+++ test/Carbunql.TypeSafe.Test/ModelNameFormatTest.cs
@@ -0,0 +1,85 @@
+﻿using Xunit.Abstractions;
+
+namespace Carbunql.TypeSafe.Test;
+
+public class ModelNameFormatTest
+{
+
+    public ModelNameFormatTest(ITestOutputHelper output)
+    {
+        Output = output;
+    }
+
+    private ITestOutputHelper Output { get; }
+
+    [Fact]
+    public void SnakeCase_OmitSelectClause()
+    {
+        var sd = Sql.DefineDataSet<StoreData>();
+
+        var query = Sql.From(() => sd);
+
+        var actual = query.ToText();
+        Output.WriteLine(actual);
+
+        var expect = @"SELECT
+    *
+FROM
+    store_data AS sd";
+
+        Assert.Equal(expect, actual, true, true, true);
+    }
+
+    [Fact]
+    public void SnakeCase_SelectAll()
+    {
+        var sd = Sql.DefineDataSet<StoreData>();
+
+        var query = Sql.From(() => sd)
+            .Select(() => sd);
+
+        var actual = query.ToText();
+        Output.WriteLine(actual);
+
+        var expect = @"SELECT
+    sd.store_data_id,
+    sd.store_name
+FROM
+    store_data AS sd";
+
+        Assert.Equal(expect, actual, true, true, true);
+    }
+
+    [Fact]
+    public void SnakeCase_SelectColumn()
+    {
+        var sd = Sql.DefineDataSet<StoreData>();
+
+        var query = Sql.From(() => sd)
+            .Select(() => new
+            {
+                sd.StoreDataId,
+                sd.StoreName,
+            });
+
+        var actual = query.ToText();
+        Output.WriteLine(actual);
+
+        var expect = @"SELECT
+    sd.store_data_id,
+    sd.store_name
+FROM
+    store_data AS sd";
+
+        Assert.Equal(expect, actual, true, true, true);
+    }
+
+    public record StoreData : IDataRow
+    {
+        public int StoreDataId { get; set; }
+        public string StoreName { get; set; } = string.Empty;
+        // interface property
+        IDataSet IDataRow.DataSet { get; set; } = null!;
+    }
+
+}
