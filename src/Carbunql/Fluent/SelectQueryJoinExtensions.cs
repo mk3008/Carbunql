a16697c562404b6d826f95606e7b58c7bd799e5a
mk3008 <mk3008.develop@gmail.com>
2024-10-12 21:46:13 +0900

    Improved the way to write table join expressions

--- src/Carbunql/Fluent/SelectQueryJoinExtensions.cs
+++ src/Carbunql/Fluent/SelectQueryJoinExtensions.cs
@@ -2,72 +2,87 @@
 
 public static class SelectQueryJoinExtensions
 {
-    public static SelectQuery InnerJoin(this SelectQuery query, FluentTable left, FluentTable right, IEnumerable<string> columns)
+    private static string ToCondition(this IEnumerable<string> columns, string leftAlias, string rightAlias)
+    {
+        return string.Join(" and ", columns.Select(x => $"{leftAlias}.{x} = {rightAlias}.{x}"));
+    }
+
+    private static SelectQuery Join(this SelectQuery query, string joinCommand, FluentTable right, string condition = "")
     {
         if (right.IsCommonTable)
         {
             query.With(right);
         }
 
-        var condition = string.Join(" and ", columns.Select(x => $"{left.Alias}.{x} = {right.Alias}.{x}"));
-        query.AddJoin("inner join", right.ToSelectable(), condition);
+        if (!string.IsNullOrEmpty(condition))
+        {
+            query.AddJoin(joinCommand, right.ToSelectable(), condition);
+        }
+        else
+        {
+            query.AddJoin(joinCommand, right.ToSelectable());
+        }
+
         return query;
+    }
+
+    public static SelectQuery InnerJoin(this SelectQuery query, string leftAlias, FluentTable right, string condition)
+    {
+        return query.Join("inner join", right, condition);
+    }
+
+    public static SelectQuery InnerJoin(this SelectQuery query, string leftAlias, FluentTable right, IEnumerable<string> columns)
+    {
+        return query.Join("inner join", right, columns.ToCondition(leftAlias, right.Alias));
+    }
+
+    public static SelectQuery InnerJoin(this SelectQuery query, FluentTable left, FluentTable right, IEnumerable<string> columns)
+    {
+        return query.InnerJoin(left.Alias, right, columns.ToCondition(left.Alias, right.Alias));
+    }
+
+    [Obsolete("Deprecated due to confusing argument definitions.")]
+    public static SelectQuery InnerJoin(this SelectQuery query, string rightTableName, string rightTableAlias, string leftTableAlias, IEnumerable<string> columns)
+    {
+        query.AddJoin("inner join", rightTableName, rightTableAlias, columns.ToCondition(leftTableAlias, rightTableAlias));
+        return query;
+    }
+
+    public static SelectQuery LeftJoin(this SelectQuery query, string leftAlias, FluentTable right, IEnumerable<string> columns)
+    {
+        return query.Join("left join", right, columns.ToCondition(leftAlias, right.Alias));
     }
 
     public static SelectQuery LeftJoin(this SelectQuery query, FluentTable left, FluentTable right, IEnumerable<string> columns)
     {
-        if (right.IsCommonTable)
-        {
-            query.With(right);
-        }
+        return query.Join("left join", right, columns.ToCondition(left.Alias, right.Alias));
+    }
 
-        var condition = string.Join(" and ", columns.Select(x => $"{left.Alias}.{x} = {right.Alias}.{x}"));
-        query.AddJoin("left join", right.ToSelectable(), condition);
-        return query;
+    [Obsolete("Deprecated due to confusing argument definitions.")]
+    public static SelectQuery LeftJoin(this SelectQuery query, string rightTableName, string rightTableAlias, string leftTableAlias, IEnumerable<string> columns)
+    {
+        return query.AddJoin("left join", rightTableName, rightTableAlias, columns.ToCondition(leftTableAlias, rightTableAlias));
+    }
+
+    public static SelectQuery RightJoin(this SelectQuery query, string leftAlias, FluentTable right, IEnumerable<string> columns)
+    {
+        return query.Join("right join", right, columns.ToCondition(leftAlias, right.Alias));
     }
 
     public static SelectQuery RightJoin(this SelectQuery query, FluentTable left, FluentTable right, IEnumerable<string> columns)
     {
-        if (right.IsCommonTable)
-        {
-            query.With(right);
-        }
+        return query.Join("right join", right, columns.ToCondition(left.Alias, right.Alias));
+    }
 
-        var condition = string.Join(" and ", columns.Select(x => $"{left.Alias}.{x} = {right.Alias}.{x}"));
-        query.AddJoin("right join", right.ToSelectable(), condition);
-        return query;
+    [Obsolete("Deprecated due to confusing argument definitions.")]
+    public static SelectQuery RightJoin(this SelectQuery query, string rightTableName, string rightTableAlias, string leftTableAlias, IEnumerable<string> columns)
+    {
+        return query.AddJoin("right join", rightTableName, rightTableAlias, columns.ToCondition(leftTableAlias, rightTableAlias));
     }
 
     public static SelectQuery CrossJoin(this SelectQuery query, FluentTable right)
     {
-        if (right.IsCommonTable)
-        {
-            query.With(right);
-        }
-
-        query.AddJoin("cross join", right.ToSelectable());
-        return query;
-    }
-
-    public static SelectQuery InnerJoin(this SelectQuery query, string rightTableName, string rightTableAlias, string leftTableAlias, IEnumerable<string> columns)
-    {
-        var condition = string.Join(" and ", columns.Select(x => $"{leftTableAlias}.{x} = {rightTableAlias}.{x}"));
-        query.AddJoin("inner join", rightTableName, rightTableAlias, condition);
-        return query;
-    }
-
-    public static SelectQuery LeftJoin(this SelectQuery query, string rightTableName, string rightTableAlias, string leftTableAlias, IEnumerable<string> columns)
-    {
-        var condition = string.Join(" and ", columns.Select(x => $"{leftTableAlias}.{x} = {rightTableAlias}.{x}"));
-        query.AddJoin("left join", rightTableName, rightTableAlias, condition);
-        return query;
-    }
-
-    public static SelectQuery RightJoin(this SelectQuery query, string rightTableName, string rightTableAlias, string leftTableAlias, IEnumerable<string> columns)
-    {
-        var condition = string.Join(" and ", columns.Select(x => $"{leftTableAlias}.{x} = {rightTableAlias}.{x}"));
-        query.AddJoin("right join", rightTableName, rightTableAlias, condition);
-        return query;
+        return query.Join("cross join", right);
     }
 
     public static SelectQuery CrossJoin(this SelectQuery query, string rightTableName, string rightTableAlias)
